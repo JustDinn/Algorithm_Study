c9ea02b1fb574dc2dbd1f31da2bd2431cd9735ed
HyoTaek <86708287+DIN-STUDIO@users.noreply.github.com>
2025-02-27 15:08:06 +0900

    [Bronze IV] Title: 세수정렬, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.swift
+++ 백준/Bronze/2752. 세수정렬/세수정렬.swift
@@ -1,6 +1,3 @@
-import Foundation
-
-let numbers = readLine()!.split(separator: " ").map { Int($0)! }
-let sortedNumbers = numbers.sorted()
-
-print(sortedNumbers.map { String($0) }.joined(separator: " "))
+var input = readLine()!.split(separator: " ").map { Int($0)! }
+input.sort()
+input.forEach { print($0, terminator: " ") }
