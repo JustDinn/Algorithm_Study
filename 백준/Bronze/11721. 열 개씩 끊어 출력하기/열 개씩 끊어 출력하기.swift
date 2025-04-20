a640e68f0193aea2122b36e28cb2113f59d325d8
HyoTaek <86708287+JustDinn@users.noreply.github.com>
2025-04-20 21:36:39 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.swift
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.swift
@@ -1,11 +1,12 @@
-import Foundation
+var count = 1
+let input = readLine()!
 
-if let input = readLine() {
-    var index = input.startIndex
-    
-    while index < input.endIndex {
-        let endIndex = input.index(index, offsetBy: 10, limitedBy: input.endIndex) ?? input.endIndex
-        print(String(input[index..<endIndex]))
-        index = endIndex
+for char in input {
+    if count == 10 {
+        print(char)
+        count = 1
+    } else {
+        print(char, terminator: "")
+        count += 1
     }
 }
