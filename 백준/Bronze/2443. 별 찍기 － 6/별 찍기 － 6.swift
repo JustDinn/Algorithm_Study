348997486af561f036fb381c0b9259a6e5a65bac
HyoTaek <86708287+JustDinn@users.noreply.github.com>
2025-10-20 18:55:51 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.swift
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.swift
@@ -1,28 +1,12 @@
-import Foundation
+let n = Int(readLine()!)!
 
-func veryInefficientStar2443(_ n: Int) {
-    for i in 0..<n {
-        var line = ""
-
-        for _ in 0..<20 { _ = UUID().uuidString.hashValue }
-
-        var spaceBuilder: [Character] = []
-        for _ in 0..<i {
-            for _ in 0..<1 { spaceBuilder.append(" ") }
+for i in 0..<n {
+    for j in 0..<2*n-1-i {
+        if j < i {
+            print(" ", terminator: "")
+        } else {
+            print("*", terminator: "")
         }
-        line += String(spaceBuilder)
-
-        let starCount = 2 * (n - i) - 1
-        var starBuilder: [Character] = []
-        for _ in 0..<starCount {
-            for _ in 0..<1 { starBuilder.append("*") }
-        }
-        line += String(starBuilder)
-
-        print(line)
     }
-}
-
-if let input = readLine(), let n = Int(input) {
-    veryInefficientStar2443(n)
+    print()
 }
