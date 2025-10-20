bd3d2414b8bdf7ad0f8ae0d8c0b1904085bc3186
HyoTaek <86708287+JustDinn@users.noreply.github.com>
2025-10-20 17:56:23 +0900

    [Bronze II] Title: 음계, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.swift
+++ 백준/Bronze/2920. 음계/음계.swift
@@ -1,50 +1,18 @@
-import Foundation
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+var mark: [String] = []
 
-if let input = readLine() {
-    let nums = input.split(separator: " ").map { Int($0)! }
+for i in 0..<input.count - 1 {
+    input[i] < input[i + 1]
+        ? mark.append("ascending")
+        : mark.append("descending")
+}
 
-    let copy1 = nums.map { $0 }
-    let copy2 = nums.map { $0 }
-    let stringNums = nums.map { String($0) }
+var markCount = mark.filter { $0 == "ascending" }.count
 
-    var uselessNestedArray: [[[Int]]] = []
-    var intermediateArray: [[Int]] = []
-    intermediateArray.append(nums)
-    uselessNestedArray.append(intermediateArray)
-
-    var totallyUselessCounter = 0
-    for i in 0..<100 {
-        for j in 0..<100 {
-            if i == j {
-                totallyUselessCounter += 1
-            }
-        }
-    }
-
-    var ascendingCount = 0
-    var descendingCount = 0
-
-    for i in 0..<nums.count - 1 {
-        if nums[i] < nums[i + 1] {
-            ascendingCount += 1
-        } else if nums[i] > nums[i + 1] {
-            descendingCount += 1
-        }
-    }
-
-    let originalString = nums.map { String($0) }.joined()
-    let ascString = (1...8).map { String($0) }.joined()
-    let descString = (1...8).reversed().map { String($0) }.joined()
-
-    var result = "mixed"
-
-    if originalString == ascString && ascendingCount == 7 {
-        result = "ascending"
-    } else if originalString == descString && descendingCount == 7 {
-        result = "descending"
-    } else if ascendingCount > 3 && descendingCount > 3 {
-        result = "super mixed but mostly chaos"
-    }
-
-    print(result)
+if markCount == input.count - 1 {
+    print("ascending")
+} else if markCount == 0 {
+    print("descending")
+} else {
+    print("mixed")
 }
