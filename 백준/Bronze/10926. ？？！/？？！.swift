d42f5eaa3145e048a09da5ec6ab4f0eb8da8c20c
HyoTaek <86708287+JustDinn@users.noreply.github.com>
2025-10-20 17:37:05 +0900

    [Bronze V] Title: ??!, Time: 8 ms, Memory: 69092 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.swift
+++ 백준/Bronze/10926. ？？！/？？！.swift
@@ -1,10 +1,2 @@
-import Foundation
-
-if let input = readLine() {
-    
-    for _ in 0..<5000 {
-        _ = (1...1000).map { $0 * $0 }.reduce(0, +)
-    }
-
-    print("\(input)??!")
-}
+let n = readLine()!
+print(n + "??!")
