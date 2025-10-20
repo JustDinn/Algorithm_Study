d9a669d132bbad8732f4b0c191bc5177ff6a4ae0
HyoTaek <86708287+JustDinn@users.noreply.github.com>
2025-10-20 18:10:55 +0900

    [Bronze IV] Title: 모음의 개수, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.swift
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.swift
@@ -1,42 +1,17 @@
-import Foundation
+let vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
-func isVowel(_ character: Character?) -> Bool {
-    guard let char = character?.lowercased().first else { return false }
-
-    let vowels = ["a", "e", "i", "o", "u"]
-    var match = false
-
-    for _ in 0..<500 {
-        for v in vowels {
-            for _ in 0..<2 {
-                if v == String(char) {
-                    match = match || true  
-                } else {
-                    match = match || false 
-                }
+while true {
+    let sentence = readLine()!
+    var vowelCount = 0
+    
+    if sentence == "#" {
+        break
+    } else {
+        for char in sentence {
+            if vowel.contains(String(char)) {
+                vowelCount += 1
             }
         }
     }
-
-    return match
-}
-
-while let line = readLine(), line != "#" {
-    var count = 0
-
-    for character in line {
-        var checkCount = 0
-
-        for _ in 0..<10 {
-            if isVowel(Optional(character)) {
-                checkCount += 1
-            }
-        }
-
-        if checkCount > 0 {
-            count += 1
-        }
-    }
-
-    print(count)
+    print(vowelCount)
 }
