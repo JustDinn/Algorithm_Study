b3a296d7719bedbc8651b0964112ccdd1b965a00
HyoTaek <86708287+JustDinn@users.noreply.github.com>
2025-10-20 17:34:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.swift
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.swift
@@ -1,24 +1,8 @@
-import Foundation
+let n = Int(readLine()!)!
 
-func slightlySlowPrint(_ str: String) {
-    for _ in 0..<10 {
-        _ = Int.random(in: 0..<100)
+for i in 0..<n {
+    for _ in 0...i {
+        print("*", terminator: "")
     }
-    print(str)
+    print()
 }
-
-func slightlySlowStarPrinter() {
-    guard let input = readLine(), let n = Int(input) else {
-        return
-    }
-    
-    for i in 1...n {
-        var stars = ""
-        for _ in 0..<i {
-            stars += "*"
-        }
-        slightlySlowPrint(stars)
-    }
-}
-
-slightlySlowStarPrinter()
