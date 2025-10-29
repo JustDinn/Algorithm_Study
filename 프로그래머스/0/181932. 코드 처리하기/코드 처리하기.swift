d7735407dcb93cf8d5e7483ada33a3c1da704467
HyoTaek <86708287+JustDinn@users.noreply.github.com>
2025-10-29 22:29:09 +0900

    [level 0] Title: 코드 처리하기, Time: 15.58 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.swift
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.swift
@@ -6,19 +6,22 @@ func solution(_ code: String) -> String {
     
     for (index, char) in code.enumerated() {
         if mode == 0 {
-            if char != "1" && index % 2 == 0 {
-                ret.append(char)
-            } else if char == "1" {
+            if char == "1" {
                 mode = 1
+            } else {
+                if index % 2 == 0 {
+                    ret += String(char)
+                }
             }
         } else {
-            if char != "1" && index % 2 != 0 {
-                ret.append(char)
-            } else if char == "1" {
+            if char == "1" {
                 mode = 0
+            } else {
+                if index % 2 != 0 {
+                    ret += String(char)
+                }
             }
         }
     }
-    
     return ret.isEmpty ? "EMPTY" : ret
 }
